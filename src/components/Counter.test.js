0c2740f7fff95bffad5f49deb62274d32997d5e1
GQSM <ms314006@hotmail.com>
2020-10-13 00:36:11 +0800

    add nwe test case

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -7,3 +7,10 @@ test("Original title is 「當前點擊次數：0」", () => {
 
   expect(title.innerHTML).toBe("當前點擊次數：0");
 });
+
+test("When I click button the title will change to 「當前點擊次數：1」", () => {
+  const { getByTestId } = render(Counter);
+  const title = getByTestId("counterInformation");
+
+  expect(title.innerHTML).toBe("當前點擊次數：1");
+});
